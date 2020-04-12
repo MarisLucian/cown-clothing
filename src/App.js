478e1e2cfbccc707c0e3bd416c5f9e264f89581f
iamlucianmaris <maris.lucian@yahoo.com>
2020-04-12 21:57:27 +0200

    implemented firebase utils,included ability to store authentication users into our firestore database

--- src/App.js
+++ src/App.js
@@ -5,38 +5,53 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop-page/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
+
     this.state = {
       currentUser: null
-    }
+    };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
+
   render() {
     return (
-      <div className="App">
-
-        <Header currentUser = {this.state.currentUser}/>
+      <div>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route path='/signin' component={SignInAndSignUpPage} />
-
         </Switch>
       </div>
     );
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -9,17 +9,40 @@ const config = {
     projectId: "crown-clothing-742ee",
     storageBucket: "crown-clothing-742ee.appspot.com",
     messagingSenderId: "1075166858109",
-    appId: "1:1075166858109:web:a3cfeeeaf5af6e51a5da14",
-    measurementId: "G-MRCH8F42QS"
+    appId: "1:1075166858109:web:a3cfeeeaf5af6e51a5da14"
 };
-
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                createdAt,
+                email,
+                displayName,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt:'select_account'});
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-export const signInWithGoogle = () => auth.signInWithPopup(provider)
 export default firebase;
